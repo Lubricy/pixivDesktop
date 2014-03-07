7c29bb656295eac7b6be9af9d84d6406254faf4a
Sandtears <me@sandtears.com>
2014-03-07 10:54:06 +0800

    优化UI，添加状态显示

--- pixivDesktop/Form1.Designer.cs
+++ pixivDesktop/Form1.Designer.cs
@@ -30,36 +30,72 @@
         {
             this.btn_toPixiv = new System.Windows.Forms.Button();
             this.pixivBrowser = new System.Windows.Forms.WebBrowser();
+            this.btn_SetAsDesk = new System.Windows.Forms.Button();
+            this.label_Statement = new System.Windows.Forms.Label();
+            this.text_Statement = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // btn_toPixiv
             // 
-            this.btn_toPixiv.Location = new System.Drawing.Point(30, 12);
+            this.btn_toPixiv.Location = new System.Drawing.Point(30, 30);
             this.btn_toPixiv.Name = "btn_toPixiv";
-            this.btn_toPixiv.Size = new System.Drawing.Size(75, 23);
+            this.btn_toPixiv.Size = new System.Drawing.Size(75, 25);
             this.btn_toPixiv.TabIndex = 0;
-            this.btn_toPixiv.Text = "Start";
+            this.btn_toPixiv.Text = "生成图片";
             this.btn_toPixiv.UseVisualStyleBackColor = true;
             this.btn_toPixiv.Click += new System.EventHandler(this.btn_toPixiv_Click);
             // 
             // pixivBrowser
             // 
-            this.pixivBrowser.Location = new System.Drawing.Point(30, 52);
+            this.pixivBrowser.Location = new System.Drawing.Point(173, 164);
             this.pixivBrowser.MinimumSize = new System.Drawing.Size(20, 20);
             this.pixivBrowser.Name = "pixivBrowser";
             this.pixivBrowser.Size = new System.Drawing.Size(2030, 900);
             this.pixivBrowser.TabIndex = 1;
             // 
+            // btn_SetAsDesk
+            // 
+            this.btn_SetAsDesk.Enabled = false;
+            this.btn_SetAsDesk.Location = new System.Drawing.Point(135, 30);
+            this.btn_SetAsDesk.Name = "btn_SetAsDesk";
+            this.btn_SetAsDesk.Size = new System.Drawing.Size(75, 25);
+            this.btn_SetAsDesk.TabIndex = 2;
+            this.btn_SetAsDesk.Text = "设为桌面";
+            this.btn_SetAsDesk.UseVisualStyleBackColor = true;
+            this.btn_SetAsDesk.Click += new System.EventHandler(this.btn_SetAsDesk_Click);
+            // 
+            // label_Statement
+            // 
+            this.label_Statement.AutoSize = true;
+            this.label_Statement.Location = new System.Drawing.Point(30, 94);
+            this.label_Statement.Name = "label_Statement";
+            this.label_Statement.Size = new System.Drawing.Size(29, 12);
+            this.label_Statement.TabIndex = 3;
+            this.label_Statement.Text = "状态";
+            // 
+            // text_Statement
+            // 
+            this.text_Statement.Enabled = false;
+            this.text_Statement.Location = new System.Drawing.Point(65, 89);
+            this.text_Statement.Name = "text_Statement";
+            this.text_Statement.Size = new System.Drawing.Size(145, 60);
+            this.text_Statement.TabIndex = 4;
+            this.text_Statement.Text = "请点击\"生成图片\"按钮";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(209, 135);
+            this.ClientSize = new System.Drawing.Size(234, 161);
+            this.Controls.Add(this.text_Statement);
+            this.Controls.Add(this.label_Statement);
+            this.Controls.Add(this.btn_SetAsDesk);
             this.Controls.Add(this.pixivBrowser);
             this.Controls.Add(this.btn_toPixiv);
             this.Name = "Form1";
             this.Text = "桌面修改器";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +103,9 @@
 
         private System.Windows.Forms.Button btn_toPixiv;
         private System.Windows.Forms.WebBrowser pixivBrowser;
+        private System.Windows.Forms.Button btn_SetAsDesk;
+        private System.Windows.Forms.Label label_Statement;
+        private System.Windows.Forms.RichTextBox text_Statement;
 
     }
 }
--- pixivDesktop/Form1.cs
+++ pixivDesktop/Form1.cs
@@ -13,48 +13,89 @@ namespace pixivDesktop
 {
     public partial class Form1 : Form
     {
-        int brCounter = 0;
+        String fileNameStr = "";
         public Form1()
         {
             InitializeComponent();
         }
 
-        // 用于控制计数器判断网页是否加载完成的方法
-        private void pixivBrowser_DecumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        // 执行JS，修正排版
+        private void execJs() 
         {
             string jsStr = "";
             jsStr = jsStr +
-                "function desktopView() {" + 
+                "function desktopView() {" +
                 "var leftNode = document.getElementsByClassName('introduction-container')[0]; " +
                 "leftNode.parentNode.removeChild(leftNode);" +
                 "var rightNode = document.getElementsByClassName('newindex-bg-container')[0];" +
                 "rightNode.style.marginLeft = 0;}";
 
-            //jsStr = "function desktopView() {document.write('123');}";
-            
             // 执行js
             HtmlElement headEl = pixivBrowser.Document.GetElementsByTagName("head")[0];
             HtmlElement scriptEl = pixivBrowser.Document.CreateElement("script");
             scriptEl.InnerHtml = jsStr;
             headEl.AppendChild(scriptEl);
             pixivBrowser.Document.InvokeScript("desktopView");
+        }
+
+        // 将时间作为文件名，保存图片到Cache文件夹中
+        private void saveImg(Bitmap desktopImg) 
+        {
+            // 生成文件名
+            fileNameStr = DateTime.Now.ToString("yyyyMMddhhmmss");
+
+            // 不存在则创建
+            if (!System.IO.Directory.Exists("Cache"))
+            {
+                System.IO.Directory.CreateDirectory("Cache");
+            }
+
+            desktopImg.Save(@"Cache\" + fileNameStr + ".bmp");
+        }
+
+        // 网站加载完成之后执行的方法
+        private void pixivBrowser_DecumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            // 执行js
+            execJs();
+
+            // 修改状态
+            text_Statement.Text = "网页加载完成，等待生成截图...";
 
             // 截图
             Bitmap desktopImg = new Bitmap(1600, 900);
             pixivBrowser.DrawToBitmap(desktopImg, new Rectangle(0, 0, 1600, 900));
-            desktopImg.Save(@"test.bmp");
 
-            // 修改结束
-            MessageBox.Show("OK");
+            // 保存截图并修改状态
+            try
+            {
+                saveImg(desktopImg);
+                text_Statement.Text = "截图成功";
+                btn_SetAsDesk.Enabled = true;
+            }
+            catch
+            {
+                text_Statement.Text = "截图失败...文件夹没有读写的权限...";
+            }
         }
 
+        // 按下"生成图片"按钮，保存图片
         private void btn_toPixiv_Click(object sender, EventArgs e)
         {
             // 将事件绑定
             pixivBrowser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(pixivBrowser_DecumentCompleted);
 
+            // 修改状态
+            text_Statement.Text = "正在加载网页...";
+
             // 打开Pixiv
             pixivBrowser.Url = new Uri("http://www.pixiv.net/");
+        }
+
+        // 按下"设置桌面"按钮，将图片设置为桌面
+        private void btn_SetAsDesk_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("功能完善中...");
         }
     }
 }
